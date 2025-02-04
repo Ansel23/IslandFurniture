9a1acedf0231b3ee258dbe6fbdfbacee61017b0e
wangZuxi <WANGZUXI.23@ichat.sp.edu.sg>
2025-02-04 13:45:48 +0800

    playwright config changes

--- playwright.config.js
+++ playwright.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  timeout: 70_000,
+  timeout: 100_000,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
